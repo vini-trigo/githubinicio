ebf3de5dd5418b3165315ed94ed0c547263a0ebd
Vinicius Trigo <viniciustrigo52@gmail.com>
2020-08-11 13:16:48 -0300

    Mensagem de boa noite

--- src/git/gg.java
+++ src/git/gg.java
@@ -5,6 +5,7 @@ public class gg {
 	public static void main(String[] args) {
 		System.out.println("Salve mundo, voltei");
 		System.out.println("Boa tarde");
+		System.out.println("Boa noite");
 
 	}
 
